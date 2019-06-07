dd4d61a70ad370430b752ded9c8b4608d6188b4f
Maik Klein <maik.klein@es-shared-mbp.local>
2019-06-07 09:22:58 +0200

    remove unneeded link flags

--- build.rs
+++ build.rs
@@ -2,10 +2,8 @@ use std::path::PathBuf;
 fn main() {
     let project_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("external");
     println!("cargo:rustc-link-search=native={}", project_dir.display());
-    println!("cargo:rustc-link-lib=framework=Foundation");
     println!("cargo:rustc-link-lib=framework=Metal");
     println!("cargo:rustc-link-lib=framework=AppKit");
-    println!("cargo:rustc-link-lib=framework=CoreGraphics");
     println!("cargo:rustc-link-lib=framework=QuartzCore");
     println!("cargo:rustc-link-lib=framework=IOKit");
     println!("cargo:rustc-link-lib=framework=IOSurface");
