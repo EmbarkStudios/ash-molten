728d5e5f59384752dbc9033950ec987357f5cbce
Maik Klein <maik.klein@embark-studios.com>
2019-06-24 09:15:56 +0200

    Don't build MoltenVK quietly

--- build_molten.sh
+++ build_molten.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/KhronosGroup/MoltenVK.git
 cd MoltenVK
 sh fetchDependencies
-make macos
+xcodebuild -project "$(XCODE_PROJ)" -scheme "$(XCODE_SCHEME_BASE) (macOS only)"
 cp Package/Release/MoltenVK/macOS/static/libMoltenVK.a ../native
