b90b4ef16988adc5403fe5db36ca2d66dd062b0d
Johan Andersson <repi@repi.se>
2019-10-29 22:21:56 +0100

    Update to MoltenVK 1.0.38

--- build.rs
+++ build.rs
@@ -29,7 +29,7 @@ mod mac {
         };
 
         // MoltenVK git tagged release to use
-        let tag = "v1.0.37";
+        let tag = "v1.0.38";
 
         let checkout_dir = Path::new(&std::env::var("OUT_DIR").expect("Couldn't find OUT_DIR"))
             .join(format!("MoltenVK-{}", tag));
