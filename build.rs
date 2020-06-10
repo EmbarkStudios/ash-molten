97884580cc5cc5ae810ba43ce950241fb8d3ee00
Johan Andersson <johan@embark-studios.com>
2020-06-10 09:47:00 +0200

    Upgrade to MoltenVK 1.0.43 (#21)

--- build.rs
+++ build.rs
@@ -28,7 +28,7 @@ mod mac {
         };
 
         // MoltenVK git tagged release to use
-        let tag = "v1.0.41";
+        let tag = "v.1.0.43";
 
         let checkout_dir = Path::new(&std::env::var("OUT_DIR").expect("Couldn't find OUT_DIR"))
             .join(format!("MoltenVK-{}", tag));
